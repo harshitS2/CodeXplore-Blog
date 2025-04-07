a07a4354da4493a305f750a3adb7ce4be62cfd46
Harshit Pandey <harshit2001pandey@gmail.com>
2025-04-07 22:32:14 +0530

    Single Post Page -p1

---
+++ Client/src/components/PostMenuActions.jsx
@@ -0,0 +1,39 @@
+const PostMenuActions = () => {
+  return (
+    <div>
+      <h1 className="mb-4 mt-8 text-sm font-medium">Actions</h1>
+      <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
+        <svg
+          width="20px"
+          height="20px"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M6.75 6L7.5 5.25H16.5L17.25 6V19.3162L12 16.2051L6.75 19.3162V6ZM8.25 6.75V16.6838L12 14.4615L15.75 16.6838V6.75H8.25Z"
+            fill="#080341"
+          />
+        </svg>
+        <span>Save this post</span>
+      </div>
+      <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="20"
+          height="20"
+          viewBox="0 0 30 30"
+        >
+          <path d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z"></path>
+        </svg>
+        <span>Delete this post</span>
+      </div>
+    </div>
+  );
+};
+
+export default PostMenuActions;
--- Client/src/routes/SinglePostPage.jsx
+++ Client/src/routes/SinglePostPage.jsx
@@ -1,5 +1,7 @@
 import Image from "../components/Image";
 import { Link } from "react-router-dom";
+import PostMenuActions from "../components/PostMenuActions";
+import Search from "../components/Search";
 const SinglePostPage = () => {
   return (
     <div className="flex flex-col gap-8">
@@ -17,11 +19,127 @@ const SinglePostPage = () => {
             <Link className="text-blogAccent">Web Design</Link>
             <span>2 days ago</span>
           </div>
-          <p className="text-gray-500 font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit asperiores, pariatur dignissimos explicabo voluptate atque illo architecto aperiam, nisi quia, molestias quidem laudantium sequi at vitae consequuntur</p>
+          <p className="text-gray-500 font-medium">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            asperiores, pariatur dignissimos explicabo voluptate atque illo
+            architecto aperiam, nisi quia, molestias quidem laudantium sequi at
+            vitae consequuntur
+          </p>
         </div>
-       
+
         <div className="hidden lg:block w-2/5">
-          <Image src="postImg.jpeg" w="600" className="rounded-xl"/>
+          <Image src="postImg.jpeg" w="600" className="rounded-xl" />
+        </div>
+      </div>
+      {/* content */}
+      <div className="flex flex-col md:flex-row gap-12">
+        {/* text */}
+        <div className="lg:text-lg flex flex-col gap-6 text-justify">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            perferendis expedita aliquid veritatis fugit ullam natus. Laudantium
+            doloribus vero qui ducimus repellat maiores similique iusto
+            consequuntur, vitae est fugit blanditiis perferendis cumque commodi
+            quam, aliquam quaerat eum dolorem dicta assumenda exercitationem
+            ullam enim. Tempora doloremque similique voluptates architecto!
+            Dicta illum quis nisi aperiam. Nulla nisi ab sed temporibus
+            perferendis repudiandae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            perferendis expedita aliquid veritatis fugit ullam natus. Laudantium
+            doloribus vero qui ducimus repellat maiores similique iusto
+            consequuntur, vitae est fugit blanditiis perferendis cumque commodi
+            quam, aliquam quaerat eum dolorem dicta assumenda exercitationem
+            ullam enim. Tempora doloremque similique voluptates architecto!
+            Dicta illum quis nisi aperiam. Nulla nisi ab sed temporibus
+            perferendis repudiandae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            perferendis expedita aliquid veritatis fugit ullam natus. Laudantium
+            doloribus vero qui ducimus repellat maiores similique iusto
+            consequuntur, vitae est fugit blanditiis perferendis cumque commodi
+            quam, aliquam quaerat eum dolorem dicta assumenda exercitationem
+            ullam enim. Tempora doloremque similique voluptates architecto!
+            Dicta illum quis nisi aperiam. Nulla nisi ab sed temporibus
+            perferendis repudiandae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            perferendis expedita aliquid veritatis fugit ullam natus. Laudantium
+            doloribus vero qui ducimus repellat maiores similique iusto
+            consequuntur, vitae est fugit blanditiis perferendis cumque commodi
+            quam, aliquam quaerat eum dolorem dicta assumenda exercitationem
+            ullam enim. Tempora doloremque similique voluptates architecto!
+            Dicta illum quis nisi aperiam. Nulla nisi ab sed temporibus
+            perferendis repudiandae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            perferendis expedita aliquid veritatis fugit ullam natus. Laudantium
+            doloribus vero qui ducimus repellat maiores similique iusto
+            consequuntur, vitae est fugit blanditiis perferendis cumque commodi
+            quam, aliquam quaerat eum dolorem dicta assumenda exercitationem
+            ullam enim. Tempora doloremque similique voluptates architecto!
+            Dicta illum quis nisi aperiam. Nulla nisi ab sed temporibus
+            perferendis repudiandae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            perferendis expedita aliquid veritatis fugit ullam natus. Laudantium
+            doloribus vero qui ducimus repellat maiores similique iusto
+            consequuntur, vitae est fugit blanditiis perferendis cumque commodi
+            quam, aliquam quaerat eum dolorem dicta assumenda exercitationem
+            ullam enim. Tempora doloremque similique voluptates architecto!
+            Dicta illum quis nisi aperiam. Nulla nisi ab sed temporibus
+            perferendis repudiandae.
+          </p>
+        </div>
+        {/* menu */}
+        <div className="px-4 h-max sticky top-8">
+          <h1 className="mb-4 text-sm font-medium">Author</h1>
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-8">
+              <Image
+                src="userImg.jpeg"
+                className="w-12 h-12 rounded-full object-cover"
+                w="48"
+                h="48"
+              />
+              <Link className="text-blogAccent">John Doe</Link>
+            </div>
+            <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur.</p>
+            <div className="flex gap-2">
+              <Link>
+                <Image src="facebook.svg" />
+              </Link>
+              <Link>
+                <Image src="instagram.svg" />
+              </Link>
+            </div>
+          </div>
+          <PostMenuActions />
+          <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
+          <div className="flex flex-col gap-2 text-sm">
+            <Link className="underline" to="/">
+              All
+            </Link>
+            <Link className="underline" to="/">
+              Development
+            </Link>
+            <Link className="underline" to="/">
+              Databases
+            </Link>
+            <Link className="underline" to="/">
+              Search Engines
+            </Link>
+            <Link className="underline" to="/">
+              Marketing
+            </Link>
+          </div>
+          <h1 className="mt-8 mb-4 text-sm font-medium">Search</h1>
+          <Search />
         </div>
       </div>
     </div>

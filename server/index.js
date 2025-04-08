7286057e9605d900abf22b1a63fe0d7d86795a8f
Harshit Pandey <harshit2001pandey@gmail.com>
2025-04-08 23:34:58 +0530

    Schema Created

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ import express from "express"
 import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
+import { connectDB } from "./lib/connectDB.js"
 const app = express();
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.status(200).send("HomePage request")
 })
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
-app.listen(3000, ()=>{
+app.listen(3000, () => {
+    connectDB();
     console.log("Server is running!");
 })

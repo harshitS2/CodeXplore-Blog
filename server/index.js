78c7afd53bffce3d845d25122a176dbd67987789
Harshit Pandey <harshit2001pandey@gmail.com>
2025-04-08 10:48:45 +0530

    Server setup

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import userRouter from "./routes/user.route.js"
+import postRouter from "./routes/post.route.js"
+import commentRouter from "./routes/comment.route.js"
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.status(200).send("HomePage request")
+})
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+app.listen(3000, ()=>{
+    console.log("Server is running!");
+})

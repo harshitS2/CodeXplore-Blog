e34ca557b3780827fd6a56c96fd3440f57b839af
Harshit Pandey <harshit2001pandey@gmail.com>
2025-04-08 10:14:43 +0530

    PostList Page

--- Client/src/routes/PostListPage.jsx
+++ Client/src/routes/PostListPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PostLists from '../components/PostLists'
+import SideMenu from '../components/SideMenu'
 
 const PostListPage = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-      PostList
+    <div className=''>
+      <h1 className='mb-8 text-2xl'>Development Blog</h1>
+      <button onClick={()=> setOpen(prev => !prev)} className='bg-blogAccent text-sm text-white px-4 py-2 rounded-2xl mb-4 md:hidden'>{open ? "Close":"Filter or Search"}</button>
+
+      <div className="flex flex-col-reverse gap-8 md:flex-row">
+        <div className="">
+          <PostLists />
+        </div>
+        <div className={`${open ? "block" : "hidden"} md:block`}>
+          <SideMenu />
+        </div>
+      </div>
     </div>
   )
 }
